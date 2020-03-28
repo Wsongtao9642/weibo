980cfbdcde04c91158b1163f8594e629b94e84ed
Wsongtao <Wsongtao9642@163.com>
2020-03-28 17:31:35 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css')}}">
-  </head>
-  <body>
+<head>
+  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
 
-    @include('layouts._header')
+<body>
+@include('layouts._header')
 
-    <div class="container">
-      @yield('content')
-      @include('layouts._footer')
-    </div>
-  </body>
+<div class="container">
+  <div class="offset-md-1 col-md-10">
+    @include('shared._messages')
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</div>
+</body>
 </html>

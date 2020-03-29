e24a3fb1114292e25e0850d617f9def80639ec99
Wsongtao <Wsongtao9642@163.com>
2020-03-29 05:04:53 +0000

    用户退出登录

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::resource('users','UsersController');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
-Route::delete('logout', 'SessionsController@destroy')->name('logout');
+Route::delete('/logout', 'SessionsController@destroy')->name('logout');

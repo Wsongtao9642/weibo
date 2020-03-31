3f87217fc21cf0957d8d0632542a27416bd2d24b
Wsongtao <Wsongtao9642@163.com>
2020-03-31 09:04:27 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StaticPagesController extends Controller
 {
-	// 主页
-    public function home()
+	public function home()
     {
-    	return view('static_page/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
     {
-    	return view('static_page/help');
+        return view('static_pages/help');
     }
 
     public function about()
     {
-    	return view('static_page/about');
+        return view('static_pages/about');
     }
 }
